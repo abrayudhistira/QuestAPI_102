3a2f14e8077fd5f9f6f55764240fd94007d08380
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:19:20 +0700

    membuat class InsertViewModel

--- app/src/main/java/com/umy/pam_api/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/umy/pam_api/ui/viewmodel/InsertViewModel.kt
@@ -1,2 +1,8 @@
 package com.umy.pam_api.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import com.umy.pam_api.repository.MahasiswaRepository
+
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+
+}
