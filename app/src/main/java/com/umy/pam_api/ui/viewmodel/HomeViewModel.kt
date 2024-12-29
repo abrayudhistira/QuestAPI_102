01e3febadcb5328c06d9c8adb34503d896b12791
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:44:27 +0700

    Membuat fungsi deleteMhs

--- app/src/main/java/com/umy/pam_api/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/umy/pam_api/ui/viewmodel/HomeViewModel.kt
@@ -1,5 +1,21 @@
 package com.umy.pam_api.ui.viewmodel
 
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
+import java.io.IOException
+
 sealed class HomeUiState {
 
+}
+
+fun deleteMhs(nim:String) {
+    viewModelScope.launch {
+        try {
+            mhs.deleteMahasiswa(nim)
+        }catch(e:IOException){
+            HomeUiState.Error
+        }catch (e:HttpException) {
+            HomeUiState.Error
+        }
+    }
 }
