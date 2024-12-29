59f78047aace016664d92715e8a1fdcee70c3381
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 23:03:47 +0700

    Membuat fungsi override Mahasiswa Applications

--- app/src/main/java/com/umy/pam_api/ui/view/PenyediaViewModel.kt
+++ app/src/main/java/com/umy/pam_api/ui/view/PenyediaViewModel.kt
@@ -1,2 +1,9 @@
 package com.umy.pam_api.ui.view
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.umy.pam_api.application.MahasiswaApplications
+
+
+fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
