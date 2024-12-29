2594bc0c51d56f29a8111fd518037ccab75ecd9a
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:46:23 +0700

    Deklarasi variabel di class HomeUiState

--- app/src/main/java/com/umy/pam_api/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/umy/pam_api/ui/viewmodel/HomeViewModel.kt
@@ -2,10 +2,13 @@ package com.umy.pam_api.ui.viewmodel
 
 import androidx.lifecycle.viewModelScope
 import coil.network.HttpException
+import com.umy.pam_api.model.Mahasiswa
 import java.io.IOException
 
 sealed class HomeUiState {
-
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
 }
 
 fun deleteMhs(nim:String) {
