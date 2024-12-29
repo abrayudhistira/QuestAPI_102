1f30f188d4489541989ac7d70411599b668f7153
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:04:17 +0700

    Membuat interface AppContainer dan override class MahasiswaContainer ke AppContainer

--- app/src/main/java/com/umy/pam_api/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/umy/pam_api/dependenciesinjection/MahasiswaContainer.kt
@@ -1,2 +1,11 @@
 package com.umy.pam_api.dependenciesinjection
 
+import com.umy.pam_api.repository.MahasiswaRepository
+
+interface AppContainer {
+    val kontakRepository : MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer {
+
+}
