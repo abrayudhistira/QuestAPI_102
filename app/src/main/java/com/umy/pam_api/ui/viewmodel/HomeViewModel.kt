482f03dcf79d9b506855e68a6b0f6215ca14ab31
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:50:37 +0700

    membuat initialize getMhs

--- app/src/main/java/com/umy/pam_api/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/umy/pam_api/ui/viewmodel/HomeViewModel.kt
@@ -1,14 +1,28 @@
 package com.umy.pam_api.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import coil.network.HttpException
 import com.umy.pam_api.model.Mahasiswa
+import com.umy.pam_api.repository.MahasiswaRepository
 import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel (private val mhs: MahasiswaRepository): ViewModel(){
+    var mhsUiState : HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
 }
 
 fun deleteMhs(nim:String) {
