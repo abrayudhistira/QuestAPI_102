2bd23a0b58d4a3be55f49bef5e781432bb7cb753
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:12:43 +0700

    Membuat Code MahasiswaApplications dengan override kedalam Application

--- app/src/main/java/com/umy/pam_api/application/MahasiswaApplications.kt
+++ app/src/main/java/com/umy/pam_api/application/MahasiswaApplications.kt
@@ -1,4 +1,14 @@
 package com.umy.pam_api.application
 
-class MahasiswaApplications {
+import android.app.Application
+import com.umy.pam_api.dependenciesinjection.AppContainer
+import com.umy.pam_api.dependenciesinjection.MahasiswaContainer
+
+class MahasiswaApplications : Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
 }
