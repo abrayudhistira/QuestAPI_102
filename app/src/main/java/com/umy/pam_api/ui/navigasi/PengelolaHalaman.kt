f90c0be4fcfe8d93f2ceb189c1c47e123b2784b4
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 23:10:58 +0700

    Membuat fungsi PengelolaHalaman

--- app/src/main/java/com/umy/pam_api/ui/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/umy/pam_api/ui/navigasi/PengelolaHalaman.kt
@@ -1,2 +1,42 @@
 package com.umy.pam_api.ui.navigasi
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.umy.pam_api.ui.view.DestinasiEntry
+import com.umy.pam_api.ui.view.DestinasiHome
+import com.umy.pam_api.ui.view.EntryMhsScreen
+import com.umy.pam_api.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
