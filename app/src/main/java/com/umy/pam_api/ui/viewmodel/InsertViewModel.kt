26d507dbf2c626b3f6b92492d4637ca1329d3b67
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:22:26 +0700

    membuat fungsi insert mahasiswa by event

--- app/src/main/java/com/umy/pam_api/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/umy/pam_api/ui/viewmodel/InsertViewModel.kt
@@ -1,8 +1,20 @@
 package com.umy.pam_api.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.umy.pam_api.model.Mahasiswa
 import com.umy.pam_api.repository.MahasiswaRepository
 
 class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
-
+    var uiState by mutableStateOf(InsertUiState())
+    private set
 }
+
+fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
