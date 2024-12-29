bef7f3baa66fc015654741c2823442b5b8faf1d8
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 23:05:18 +0700

    Membuat object PenyediaViewModel

--- app/src/main/java/com/umy/pam_api/ui/view/PenyediaViewModel.kt
+++ app/src/main/java/com/umy/pam_api/ui/view/PenyediaViewModel.kt
@@ -2,8 +2,18 @@ package com.umy.pam_api.ui.view
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.umy.pam_api.application.MahasiswaApplications
+import com.umy.pam_api.ui.viewmodel.HomeViewModel
+import com.umy.pam_api.ui.viewmodel.InsertViewModel
 
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiKontak().container.kontakRepository) }
+        initializer { InsertViewModel(aplikasiKontak().container.kontakRepository) }
+    }
+}
 
 fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
