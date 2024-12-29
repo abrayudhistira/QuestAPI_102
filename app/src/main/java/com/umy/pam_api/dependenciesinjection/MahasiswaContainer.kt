8356043beeb34e5603d680b75b0049636cf38e64
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 21:07:48 +0700

    Membuat variabel dan override kontak repo kedalam mahasiswa repo

--- app/src/main/java/com/umy/pam_api/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/umy/pam_api/dependenciesinjection/MahasiswaContainer.kt
@@ -1,11 +1,30 @@
 package com.umy.pam_api.dependenciesinjection
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.umy.pam_api.repository.MahasiswaRepository
+import com.umy.pam_api.repository.NetworkMahasiswaRepository
+import com.umy.pam_api.service_api.MahasiswaService
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer {
     val kontakRepository : MahasiswaRepository
 }
 
 class MahasiswaContainer : AppContainer {
+    private val baseUrl = ""
+    private val json = Json { ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
 
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository (mahasiswaService)
+    }
 }
