f5ff60ab1cafb5dece4c5e6325caa6151ba852e5
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-29 22:44:56 +0700

    Membuat fungsi MhsLayout

--- app/src/main/java/com/umy/pam_api/ui/view/HomeView.kt
+++ app/src/main/java/com/umy/pam_api/ui/view/HomeView.kt
@@ -1,11 +1,15 @@
 package com.umy.pam_api.ui.view
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
@@ -19,6 +23,33 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.umy.pam_api.model.Mahasiswa
+
+@Composable
+fun MhsLayout(
+    mahasiswa: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick:(Mahasiswa)->Unit,
+    onDeleteClick: (Mahasiswa) -> Unit = {}
+){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(mahasiswa){ mahasiswa ->
+            MhsCard(
+                mahasiswa = mahasiswa,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable{onDetailClick(mahasiswa)},
+                onDeleteClick ={
+                    onDeleteClick(mahasiswa)
+                }
+            )
+        }
+    }
+}
+
 
 @Composable
 fun MhsCard(
